bf64150b2e9f8b1e4802a355af64dfe14cab8360
SayyedTaheraEhsan <s.tahera123@gmail.com>
2025-09-14 23:36:00 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,17 @@
+# Tuple example
+t = (3, 9, 1, 7, 9, 3, 9)
+
+# 1. count()
+print("Count of 9:", t.count(9))
+
+# 2. index()
+print("Index of 7:", t.index(7))
+
+# 3. len()
+print("Length of tuple:", len(t))
+
+# 4. max()
+print("Maximum value:", max(t))
+
+# 5. min()
+print("Minimum value:", min(t))
