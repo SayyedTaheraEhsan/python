65ec316b5662dee97395c63706c6db1c42caa5a8
SayyedTaheraEhsan <s.tahera123@gmail.com>
2025-10-15 23:10:35 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,19 +5,14 @@ student = {
     "course": "Math",
     "marks": 85
 }
-
 # 1. keys()
 print("Keys:", student.keys())
-
 # 2. values()
 print("Values:", student.values())
-
 # 3. items()
 print("Items:", student.items())
-
 # 4. get()
 print("Get age:", student.get("age"))
-
 # 5. update()
 student.update({"marks": 90})  # update marks
 print("After update:", student)
